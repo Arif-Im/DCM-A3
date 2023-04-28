21fb313dbad8ab8e936af0847258c66971fcdd2d
SInt-Win11 <throwawayspectacle@gmail.com>
2023-04-28 09:39:19 +0800

    Turn Timer

--- Assets/DataComm/Scripts/DCMTurnManager.cs
+++ Assets/DataComm/Scripts/DCMTurnManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class DCMTurnManager : NetworkBehaviour
@@ -8,25 +10,42 @@ public class DCMTurnManager : NetworkBehaviour
     public static DCMTurnManager Instance { get; private set; }
 
     public GameObject TurnMarker;
+    public TextMeshProUGUI turnText;
+    public int TurnIndex;
 
-    [SerializeField] private GameTimer _gameTimer;
-    private void Awake() 
-    { 
+    public float TurnTime = 10f;
+
+    public float TurnNetworkBeginTime;
+
+    public float curTurnTimeLeft;
+
+    private GameTimer _gameTimer;
+
+    private void Awake()
+    {
         // If there is an instance, and it's not me, delete myself.
-    
-        if (Instance != null && Instance != this) 
-        { 
-            Destroy(this); 
-        } 
-        else 
-        { 
-            Instance = this; 
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
         }
 
-        TryGetComponent(out _gameTimer);
+        turnText.text = $"<size=130%>Player {TurnIndex}'s </size>\nTurn\n{curTurnTimeLeft.ToString("F2")}";
     }
-    
-    
-    
-    
+
+    private void Update()
+    {
+        if (curTurnTimeLeft >= 10)
+        {
+            TurnNetworkBeginTime = (float)NetworkTime.time;
+            TurnIndex++;
+        }
+
+        curTurnTimeLeft = (float)NetworkTime.time - TurnNetworkBeginTime;
+        turnText.text = $"<size=130%>Player {TurnIndex}'s </size>\nTurn\n{curTurnTimeLeft.ToString("F2")}";
+    }
 }
